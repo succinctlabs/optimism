81bd9884262264e850e68687506cb460ba25355e
mariuspod <14898268+mariuspod@users.noreply.github.com>
2024-06-01 01:34:40 +0200

    feat: replace boolean flag with int flag to specify max retries when there is an outdated consensus context

--- proxyd/proxyd.go
+++ proxyd/proxyd.go
@@ -349,8 +349,8 @@ func Start(config *Config) (*Server, func(), error) {
 			if bgcfg.ConsensusPollerInterval > 0 {
 				copts = append(copts, WithPollerInterval(time.Duration(bgcfg.ConsensusPollerInterval)))
 			}
-			if bgcfg.ConsensusPollerRetry {
-				copts = append(copts, WithPollerRetry(true))
+			if bgcfg.ConsensusMaxRetries > 0 {
+				copts = append(copts, WithConsensusMaxRetries(bgcfg.ConsensusMaxRetries))
 			}
 
 			var tracker ConsensusTracker
