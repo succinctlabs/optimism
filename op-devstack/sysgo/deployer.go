0870bbdcadd6eadd38f23348cf6939cd1a86c67a
Farhad Shabani <Farhad.Shabani@gmail.com>
2025-12-23 09:44:47 -0800

    feat: add ANCHOR_STATE_REGISTRY_ADDRESS (#324)

--- op-devstack/sysgo/deployer.go
+++ op-devstack/sysgo/deployer.go
@@ -180,6 +180,7 @@ func WithDeployer() stack.Option[*Orchestrator] {
 
 type L2Deployment struct {
 	systemConfigProxyAddr          common.Address
+	anchorStateRegistry            common.Address
 	disputeGameFactoryProxy        common.Address
 	l1StandardBridgeProxy          common.Address
 	proxyAdmin                     common.Address
